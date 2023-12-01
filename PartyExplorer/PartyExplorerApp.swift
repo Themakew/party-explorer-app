1264b7de4318b6fdb8d3276b3c31331688154604
Ruyther Costa <rcosta.datum@digio.com.br>
2023-12-01 13:00:14 -0700

    Adding xcode project

---
+++ PartyExplorer/PartyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyExplorerApp.swift
+//  PartyExplorer
+//
+//  Created by Ruyther Costa on 2023-12-01.
+//
+
+import SwiftUI
+
+@main
+struct PartyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
